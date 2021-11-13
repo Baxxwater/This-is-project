73654b832503929e12eacb2d031e3d8c1cb44a22
Baxxwater <justin3483873@gmail.com>
2021-11-13 18:26:44 +0800

    20211113 hola

---
+++ I am not project 20211113/20211113.py
@@ -0,0 +1,11 @@
+
+print ('you %d SB'%94)
+print ('you %3d SB'%94)
+print ('you %03d SB'%94)
+print ('you {} SB'.format(94))
+print ('{0:d} {1:3d} {2:03d}'.format (90,80,70))
+print(type(True))
+print(type("hoola"))
+
+ans = input('請輸入號碼:')
+print(type(ans))
