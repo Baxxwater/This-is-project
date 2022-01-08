5a2b9095eb42f8c11f644fd61d0a71579d8cb5ee
Baxxwater <justin3483873@gmail.com>
2022-01-08 18:48:59 +0800

    only me ?

---
+++ 20220108/20220108 what.py
@@ -0,0 +1,39 @@
+import turtle  
+#for i in range(5):    
+   # for t in range(4):
+    #    turtle.forward(100)
+    #    turtle.right(90)
+    #turtle.left(90)
+    #for e in range(4):    
+    #    turtle.backward(100)
+    #    turtle.left(90)
+    #turtle.right(90)
+#turtle.done()
+
+# turtle.penup()
+# turtle.color('red')
+# turtle.shape('turtle')
+# for t in range(0,501,3):
+#     turtle.stamp()
+#     turtle.right(30)
+#     turtle.forward(t)
+# turtle.done()
+
+# for y in range(5):
+#     turtle.stamp()
+#     turtle.forward(100)
+#     turtle.right(144)
+    
+# turtle.done()
+a = '蘋果' 
+b = '柳橙'
+c = '葡萄'
+x = int(input("請輸入編號:")) 
+while x != "exit":
+    if x == 1:
+        print(a)
+    elif x == 2:
+        print(b)
+    elif x == 3:
+        print(c)
+    
